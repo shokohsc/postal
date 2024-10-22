dc8ae448426bf649cbeb8f2cb06d3c8c6aa03b59
shokohsc <shokohsc@gmail.com>
2024-10-22 13:52:09 +0200

    chore(): added breadcrumbs nav

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,11 @@
         </q-btn>
 
         <q-toolbar-title>
-          <router-link to="/" class="text-white" style="text-decoration: none;" >Postal</router-link>
+          <!-- <router-link to="/" class="text-white" style="text-decoration: none;" >{{ title }}</router-link> -->
+          <q-breadcrumbs separator="->" active-color="white">
+            <q-breadcrumbs-el label="Postal" to="/" />
+            <q-breadcrumbs-el v-if="title" :label=title :to="'/' + title" />
+          </q-breadcrumbs>
         </q-toolbar-title>
         <Search />
       </q-toolbar>
@@ -32,11 +36,13 @@ import 'virtual:fonts.css'
 import Mailboxes from './components/Mailboxes.vue'
 // import Draft from './components/Draft.vue'
 import Search from './components/Search.vue'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useQuasar } from 'quasar'
 import { storeToRefs } from 'pinia'
+import { useRoute } from 'vue-router'
 import { useEmailStore } from './stores/email'
 
+const route = useRoute()
 const { unreadMailNumber } = storeToRefs(useEmailStore())
 const leftDrawerOpen = ref(false)
 const toggleLeftDrawer = () => {
@@ -44,6 +50,7 @@ const toggleLeftDrawer = () => {
 }
 const $q = useQuasar()
 $q.dark.set(true)
+const title = computed(() => route.params.mailbox ? route.params.mailbox : false)
 </script>
 
 <style>
